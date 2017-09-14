7d5e0f9b49ec42cdd3394e8885c76311b6bb031d
Jeremiah Langner <jeremiahlangner@Jeremiahs-Mac-mini.local>
2017-09-14 18:23:52 -0400

    implement design changes.

--- js/handle-input.js
+++ js/handle-input.js
@@ -1,6 +1,5 @@
 function handleInput() {
-	var firstName = $('#first-name').val();
-	var lastName = $('#last-name').val();
+	var fullName = $('#full-name').val();
 	var address = $('#street-address').val();
 	var addressTwo = $('street-address-2').val() || '';
 	var city = $('#city').val();
@@ -15,9 +14,10 @@ function handleInput() {
 	var comments = $('#comments').val() || '';
 
 	var contactEmail = /* $('contact-email').val(); */ 'jeremiah@jeremiahlangner.com';
+	var from = 'fifthofeight@yahoo.com'
 	var apiKey = '';
 
-	var newApplicant = 'First Name: ' + firstName + '\n' + 'Last Name: ' + lastName + '\n' + 'Address 1: ' + address + '\n' + 'Address 2: ' + addressTwo + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip: ' + zip + '\n' + 'Phone Number: ' + phoneNumber + '\n' + 'Phone Type: ' + phoneType + '\n' + 'Email: ' + email + '\n' + 'Loan Amount Requested: ' + loanAmount + '\n' + 'Best Time to Contact: ' + time + '\n' + 'How did you hear about us?: ' + reason + '\n' + 'Additional Customer Comments: ' + comments;
+	var newApplicant = 'Name: ' + fullName + '\n' + 'Last Name: ' + lastName + '\n' + 'Address 1: ' + address + '\n' + 'Address 2: ' + addressTwo + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip: ' + zip + '\n' + 'Phone Number: ' + phoneNumber + '\n' + 'Phone Type: ' + phoneType + '\n' + 'Email: ' + email + '\n' + 'Loan Amount Requested: ' + loanAmount + '\n' + 'Best Time to Contact: ' + time + '\n' + 'How did you hear about us?: ' + reason + '\n' + 'Additional Customer Comments: ' + comments;
 
-	$.post('https://dry-retreat-60525.herokuapp.com', { apiKey: apiKey, to: contactEmail, contents: newApplicant });
+	$.post('https://dry-retreat-60525.herokuapp.com', { apiKey: apiKey, from: from, to: contactEmail, contents: newApplicant });
 }
