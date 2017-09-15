129f5ad4c442f5693f0a32520199e4ae0f68c2e4
Jeremiah Langner <jeremiahlangner@Jeremiahs-Mac-mini.local>
2017-09-15 01:09:22 -0400

    update email cred.

--- js/handle-input.js
+++ js/handle-input.js
@@ -161,11 +161,11 @@ function handleInput() {
 	var reason = $('#reason').val();
 	var comments = $('#comments').val() || '';
 
-	var contactEmail = /* $('contact-email').val(); */ 'jeremiah@jeremiahlangner.com';
+	var to = /* $('contact-email').val(); */ 'jeremiah@jeremiahlangner.com';
 	var from = 'fifthofeight@yahoo.com'
 	var apiKey = '';
 
-	var newApplicant = 'Name: ' + fullName + '\n' + 'Last Name: ' + lastName + '\n' + 'Address 1: ' + address + '\n' + 'Address 2: ' + addressTwo + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip: ' + zip + '\n' + 'Phone Number: ' + phoneNumber + '\n' + 'Phone Type: ' + phoneType + '\n' + 'Email: ' + email + '\n' + 'Loan Amount Requested: ' + loanAmount + '\n' + 'Best Time to Contact: ' + time + '\n' + 'How did you hear about us?: ' + reason + '\n' + 'Additional Customer Comments: ' + comments;
+	var contents = 'Name: ' + fullName + '\n' + 'Last Name: ' + lastName + '\n' + 'Address 1: ' + address + '\n' + 'Address 2: ' + addressTwo + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip: ' + zip + '\n' + 'Phone Number: ' + phoneNumber + '\n' + 'Phone Type: ' + phoneType + '\n' + 'Email: ' + email + '\n' + 'Loan Amount Requested: ' + loanAmount + '\n' + 'Best Time to Contact: ' + time + '\n' + 'How did you hear about us?: ' + reason + '\n' + 'Additional Customer Comments: ' + comments;
 
-	$.post('https://dry-retreat-60525.herokuapp.com', { apiKey: apiKey, from: from, to: contactEmail, contents: newApplicant });
+	$.post('https://dry-retreat-60525.herokuapp.com', { apiKey: apiKey, from: from, to: to, contents: contents });
 }
