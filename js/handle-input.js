1fb9620532ff0f36e856f21ace970acc9e399dfd
Jeremiah Langner <jeremiahlangner@Jeremiahs-Mac-mini.local>
2017-09-14 21:43:43 -0400

    consolidate input scripts

--- js/handle-input.js
+++ js/handle-input.js
@@ -1,3 +1,151 @@
+/* Form validation */
+(function() {
+  "use strict";
+  window.addEventListener("load", function() {
+    var form = document.getElementById("needs-validation");
+    form.addEventListener("submit", function(event) {
+      if (form.checkValidity() == false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+      if (form.checkValidity() == true) {
+        handleInput();
+      }
+    }, false);
+  }, false);
+}());
+
+/* Advance through forms */
+function advanceStep() {
+	var currentStep = document.querySelector('.form-step');
+	var finding = true;
+
+	while(finding) {
+		if($(currentStep).css('display') == 'block') {
+			console.log(currentStep);
+			$(currentStep).toggle();
+			$(currentStep).next().toggle();
+			break;	
+		} else {
+			currentStep = $(currentStep).next();
+		}
+	}
+}
+
+function retreatStep() {
+	var currentStep = document.querySelector('.form-step');
+	var finding = true;
+
+	while(finding) {
+		if($(currentStep).css('display') == 'block') {
+			console.log(currentStep);
+			$(currentStep).toggle();
+			$(currentStep).prev().toggle();
+			break;	
+		} else {
+			currentStep = $(currentStep).next();
+		}
+	}
+}
+
+/* Zip Code Query */
+function handleResp(data) {
+	// Check for error
+	if (data.error_msg)
+		console.log(data.error_msg);
+	// else if ("distance" in data)
+	// {
+	// 	// Show div
+	// 	container.find("div.distance").show()
+	// 	// Set distance
+	// 	.find("span.distance").text(data.distance + " miles");
+	// }
+	else {
+		var zipList = data.zip_codes;
+		console.log(zipList);
+
+		// cache queried zipcode
+		var queryZip = {};
+		zipList.forEach(function(zipcode) {
+			if(zipcode.distance == 0) {
+				queryZip = zipcode;
+			}
+		});
+
+		// sort the zipcode list from closest to furthest
+		zipList.sort(function(a, b) {
+		    return parseFloat(a.distance) - parseFloat(b.distance);
+		});
+
+		var htmlString = '';
+		
+		// Build html for list.
+		zipList.slice(0, 6).forEach(function(zipcode) {
+			if(zipcode.state = queryZip.state) {
+				htmlString = htmlString + 
+					'<ul style="list-style-type:none; padding-left: 0;"><li>' + 
+					zipcode.city + ', ' + zipcode.state + 
+					'</li><li>Zipcode: ' + zipcode.zip_code + 
+					'</li><li>Distance: ';
+				if (zipcode.distance == 0) {
+					htmlString = htmlString + '<' + zipList[1].distance.toFixed() + ' mi.</li></ul>';
+				} else {
+					htmlString = htmlString + '~' + zipcode.distance.toFixed(1) + ' mi.</li></ul>';
+				}	
+			}
+		});
+		// Add list to page.
+		$('.location-header').show();
+		$('#zipcode-info').html(htmlString);
+	}
+}
+
+function searchForZip () {
+	var clientKey = "js-0RdwQ3vrCaId1vH7GQzG7V0m5sqJdFLexZLn2gOFrxTt2AAIr9oz5oWaRnT52Fkz";
+	
+	var cache = {};
+	var zipcode = $('#zip-search').val().substring(0, 5); //$("input[name='zipcode']").val().substring(0, 5);
+	if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode)) {
+		// Clear error
+		// errorDiv.empty();
+		// Check cache
+		var cacheKey = zipcode;
+		if (cacheKey in cache) {
+			handleResp(cache[cacheKey]);
+		} else {
+			// Build url
+			// https://zipcodedistanceapi.redline13.com/rest/<api_key>/radius.csv/08057/5/miles?minimal
+			var url = "https://www.zipcodeapi.com/rest/"+clientKey+"/radius.json/" + zipcode + "/15/miles";
+			
+			// Make AJAX request
+			$.ajax({
+				"url": url,
+				"dataType": "json"
+			}).done(function(data) {
+				handleResp(data);
+				
+				// Store in cache
+				cache[cacheKey] = data;
+
+			}).fail(function(data) {
+				if (data.responseText && (json = $.parseJSON(data.responseText)))
+				{
+					// Store in cache
+					cache[cacheKey] = json;
+					
+					// Check for error
+					if (json.error_msg)
+						console.log(json.error_msg);
+				}
+				else
+					console.log('Request failed.');
+			});
+		}
+	}
+}
+
+/* Form Submission */
 function handleInput() {
 	var fullName = $('#full-name').val();
 	var address = $('#street-address').val();
