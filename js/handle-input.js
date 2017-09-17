2bd5168bb033d7e4b309a4d9f7c222702069b1b0
Jeremiah Langner <jeremiahlangner@Jeremiahs-Mac-mini.local>
2017-09-17 12:27:06 -0400

    begin removing jquery and cleaning js

--- js/handle-input.js
+++ js/handle-input.js
@@ -1,5 +1,19 @@
-/* Form validation */
-(function() {
+/* Some shorthand. */
+function value(el) {
+	return document.querySelector(el).value || '';
+}
+
+function selectEl(el) {
+	return document.querySelector(el);
+}
+
+/* function handleEvents() {
+	document.getElementsByTagName("BUTTON").addEventListener("click", advanceFormStep());
+	document.getElementsByClassName("previous").addEventListener("click", retreatFormStep());
+	docmuent.
+} */
+
+function validateInput() {
 	var els = document.getElementsByTagName("INPUT");
 	var submitButton = document.querySelector(".submit-button");
 
@@ -21,27 +35,9 @@
 			}
 		})
 	});
-})();
-
-function sendForm() {
-	var form = document.getElementById("needs-validation");
-	
-	if (form.checkValidity() == false) {
-		/*event.preventDefault();
-		event.stopPropagation(); */
-		var submitButton = document.querySelector(".submit-button");
-		submitButton.classList.add("animated");
-		submitButton.classList.add("shake");
-	}
-	
-	if (form.checkValidity() == true) {
-		$('#thanks-modal').modal('show');
-		handleSend();
-	}
 }
 
-/* Advance through forms */
-function advanceStep() {
+function advanceFormStep() {
 	var currentStep = document.querySelector('.form-step');
 	var finding = true;
 
@@ -56,7 +52,7 @@ function advanceStep() {
 	}
 }
 
-function retreatStep() {
+function retreatFormStep() {
 	var currentStep = document.querySelector('.form-step');
 	var finding = true;
 
@@ -71,24 +67,16 @@ function retreatStep() {
 	}
 }
 
-/* Zip Code Query */
-function handleResp(data) {
+function handleZipResp(data) {
 	// Check for error
-	if (data.error_msg)
+	if (data.error_msg) {
 		console.log(data.error_msg);
-	// else if ("distance" in data)
-	// {
-	// 	// Show div
-	// 	container.find("div.distance").show()
-	// 	// Set distance
-	// 	.find("span.distance").text(data.distance + " miles");
-	// }
-	else {
+	} else {
 		var zipList = data.zip_codes;
-		console.log(zipList);
 
 		// cache queried zipcode
 		var queryZip = {};
+		
 		zipList.forEach(function(zipcode) {
 			if(zipcode.distance == 0) {
 				queryZip = zipcode;
@@ -111,7 +99,7 @@ function handleResp(data) {
 					'</li><li>Zipcode: ' + zipcode.zip_code + 
 					'</li><li>Distance: ';
 				if (zipcode.distance == 0) {
-					htmlString = htmlString + '<' + zipList[1].distance.toFixed() + ' mi.</li></ul>';
+					htmlString = htmlString + '<' + math.round(zipList[1].distance) + ' mi.</li></ul>';
 				} else {
 					htmlString = htmlString + '~' + zipcode.distance.toFixed(1) + ' mi.</li></ul>';
 				}	
@@ -123,29 +111,25 @@ function handleResp(data) {
 	}
 }
 
-function searchForZip () {
+function searchForZip() {
 	var clientKey = "js-0RdwQ3vrCaId1vH7GQzG7V0m5sqJdFLexZLn2gOFrxTt2AAIr9oz5oWaRnT52Fkz";
 	
 	var cache = {};
-	var zipcode = $('#zip-search').val().substring(0, 5); //$("input[name='zipcode']").val().substring(0, 5);
+	var zipcode = value('#zip-search').substring(0, 5);
 	if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode)) {
-		// Clear error
-		// errorDiv.empty();
 		// Check cache
 		var cacheKey = zipcode;
 		if (cacheKey in cache) {
-			handleResp(cache[cacheKey]);
+			handleZipResp(cache[cacheKey]);
 		} else {
-			// Build url
-			// https://zipcodedistanceapi.redline13.com/rest/<api_key>/radius.csv/08057/5/miles?minimal
 			var url = "https://www.zipcodeapi.com/rest/"+clientKey+"/radius.json/" + zipcode + "/15/miles";
 			
-			// Make AJAX request
+			// Request from API
 			$.ajax({
 				"url": url,
 				"dataType": "json"
 			}).done(function(data) {
-				handleResp(data);
+				handleZipResp(data);
 				
 				// Store in cache
 				cache[cacheKey] = data;
@@ -157,36 +141,36 @@ function searchForZip () {
 					cache[cacheKey] = json;
 					
 					// Check for error
-					if (json.error_msg)
+					if (json.error_msg) {
 						console.log(json.error_msg);
+					}
 				}
-				else
+				else {
 					console.log('Request failed.');
+				}
 			});
 		}
 	}
 }
 
-/* Form Submission */
 function handleSend() {
-	var fullName = $('#full-name').val();
-	var address = $('#street-address').val();
-	var addressTwo = $('#street-address-2').val() || '';
-	var city = $('#city').val();
-	var state = $('#state').val();
-	var zip = $('#zipcode').val();
-	var phoneNumber = $('#phone-number').val();
-	var phoneType = $('#phone-type').val();
-	var email = $('#email').val();
-	var loanAmount = $('#loan-amount').val();
-	var time = $('#contact-time').val() + ' ' + $('#day-of-the-week').val();
-	var reason = $('#reason').val();
-	var comments = $('#comments').val() || '';
 
 	var to = 'jeremiah@jeremiahlangner.com';
 	var from = 'fifthofeight@yahoo.com';
 
-	var contents = 'Name: ' + fullName + '\n' + 'Address 1: ' + address + '\n' + 'Address 2: ' + addressTwo + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip: ' + zip + '\n' + 'Phone Number: ' + phoneNumber + '\n' + 'Phone Type: ' + phoneType + '\n' + 'Email: ' + email + '\n' + 'Loan Amount Requested: ' + loanAmount + '\n' + 'Best Time to Contact: ' + time + '\n' + 'How did you hear about us?: ' + reason + '\n' + 'Additional Customer Comments: ' + comments;
+	var contents = 'Name: ' + value('#full-name') + '\n' + 
+		'Address 1: ' + value('#street-address') + '\n' + 
+		'Address 2: ' + value('#street-address-2') + '\n' + 
+		'City: ' + value('#city') + '\n' + 
+		'State: ' + value('#state') + '\n' + 
+		'Zip: ' + value('#zipcode') + '\n' + 
+		'Phone Number: ' + value('#phone-number') + '\n' + 
+		'Phone Type: ' + value('#phone-type') + '\n' + 
+		'Email: ' + value('#email') + '\n' + 
+		'Loan Amount Requested: ' + value('#loan-amount') + '\n' + 
+		'Best Time to Contact: ' + value('#contact-time') + ' ' + value('#day-of-the-week') + '\n' + 
+		'How did you hear about us?: ' + value('#reason') + '\n' + 
+		'Additional Customer Comments: ' + value('#comments');
 
 	$.post('https://dry-retreat-60525.herokuapp.com', { from: from, to: to, contents: contents })
 		.done(function() {
@@ -197,8 +181,31 @@ function handleSend() {
 		})
 		.always(function() {
 			setTimeout(function() {
-				$('#thanks-modal').hide();
-				}, 2000);	
+				$('#thanks-modal').toggle();
+				}, 3000);	
 		});
 	
 }
+
+function checkSend() {
+	var form = document.getElementById("needs-validation");
+	
+	if (form.checkValidity() == false) {
+		/*event.preventDefault();
+		event.stopPropagation(); */
+		var submitButton = document.querySelector(".submit-button");
+		submitButton.classList.add("animated");
+		submitButton.classList.add("shake");
+	}
+	
+	if (form.checkValidity() == true) {
+		$('#thanks-modal').modal('show');
+		handleSend();
+	}
+}
+
+// handleEvents();
+(function() {
+	validateInput();
+})();
+
